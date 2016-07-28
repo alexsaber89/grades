6ad950a7a05eb53b8d6d515452f534d35ea8482f
Alex Saber <Alex Saber>
2016-07-27 21:58:30 -0500

    added for loop/if statement to determine count of grades and utilized array.reduce to determin lowest/highest grades

--- grades.js
+++ grades.js
@@ -4,7 +4,6 @@ var grade_B = 0;
 var grade_C = 0;
 var grade_D = 0;
 var grade_F = 0;
-// Make array of grades?
 
 // Loop over an array of student grades (values from 50-100) and outputs how many of each grades there are.
 // A score of 50-60 is an F
@@ -28,5 +27,17 @@ for (var i = 0; i < scores.length; i++) {
 
 // Use console.log to output the following criteria:
   // How many of each grade?
+console.log("There were " + grade_A + " A's, " + grade_B + " B's, "
+  + grade_C + " C's, " + grade_D + " D's, and " + grade_F + " F's.")
+
   // What is the lowest grade?
+var lowestGrade = scores.reduce(function(a,b) {
+  return Math.min(a,b);
+});
+console.log("The lowest grade was a " + lowestGrade + ".");
+
   // What is the highest grade?
+var highestGrade = scores.reduce(function(a,b) {
+  return Math.max(a,b);
+});
+console.log("The highest grade was a " + highestGrade + ".");
